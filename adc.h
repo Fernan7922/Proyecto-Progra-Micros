f364d5ef1b8d5db1cee58b18310933d4ef153c93
Fernan7922 <161558926+Fernan7922@users.noreply.github.com>
2026-05-20 15:17:24 -0600

    Update adc.h

--- adc.h
+++ adc.h
@@ -1,14 +1,33 @@
 /*
  * adc.h
  * IE2023 - Proyecto 2 Etapa 2
- * Autor: Fernando Jos� Guzm�n 24734
+ * Autor: Fernando Jose Guzman 24734
  */
+
 #ifndef ADC_H_
 #define ADC_H_
 
 #include <avr/io.h>
 
-void     adc_init(void);
+/*
+ * Inicializa el módulo ADC del ATmega328P.
+ *
+ * Configura:
+ * - referencia AVcc
+ * - prescaler
+ * - habilitación del ADC
+ */
+void adc_init(void);
+
+/*
+ * Lee un canal analógico específico.
+ *
+ * Parámetro:
+ * canal -> canal ADC a utilizar
+ *
+ * Retorna:
+ * valor de 10 bits entre 0 y 1023
+ */
 uint16_t adc_leer(uint8_t canal);
 
 #endif
